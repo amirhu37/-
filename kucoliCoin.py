e69378894900c8b317c0ab942a089e5ded78c013
Amir <88682289+amirhu37@users.noreply.github.com>
2021-12-15 21:08:58 +0330

    bug Fixed, cheers ;))

--- kucoliCoin.py
+++ kucoliCoin.py
@@ -8,13 +8,16 @@ driver.get('https://coinmarketcap.com/exchanges/kucoin/')
 names = list()
 driver.execute_script("window.scrollTo(0, window.scrollY + 5000)")
 # Render The Pge
-for j in range(1, 9):
-    for i in range(3):
-        driver.execute_script("window.scrollTo(0, window.scrollY + 2000)")
-        sleep(.5)
-    driver.find_element_by_xpath(
-        '//*[@id="__next"]/div[1]/div/div[2]/div/div[3]/div[2]/button').click()
-
+while True:
+    try:
+        for i in range(3):
+            driver.execute_script("window.scrollTo(0, window.scrollY + 2000)")
+            sleep(.5)
+        driver.find_element_by_xpath(
+            '//*[@id="__next"]/div[1]/div/div[2]/div/div[3]/div[2]/button').click()
+    except:
+        break
+        
 sleep(2)
 driver.execute_script("document.documentElement.scrollTop = 500;")
 
@@ -26,11 +29,14 @@ driver.execute_script(
     """document.evaluate('//*[@id="tippy-3"]/div/div[1]/div/div/button[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();""")
 
 
-for i in range(0,420):
-    s = driver.find_element_by_xpath(
-        f'//*[@id="__next"]/div[1]/div/div[2]/div/div[3]/div[1]/div/table/tbody/tr[{i}]/td[3]/div/a').get_attribute('innerHTML')
-    names.append(s)
-
+while True:
+    i += 1
+    try:
+        s = driver.find_element_by_xpath(
+            f'//*[@id="__next"]/div[1]/div/div[2]/div/div[3]/div[1]/div/table/tbody/tr[{i}]/td[3]/div/a').get_attribute('innerHTML')
+        names.append(s)
+    except:
+        break
 
 with open('kucoli-coin.txt', 'w', newline='\n') as f:
     for i in range(len(names)):
